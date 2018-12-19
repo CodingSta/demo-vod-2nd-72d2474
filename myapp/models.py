6b2233c43ee0d0b569fa1fcb7f8c27375ea1d4ac
Chinseok Lee <me@askcompany.kr>
2018-12-19 07:31:52 +0700

    EP05: ModelForm

--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,13 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=20)
+    message = models.TextField()
+    ip = models.CharField(max_length=15)
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import redirect, render
-from .forms import PostForm
-from .models import Post
+from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, redirect, render
+from .forms import PostForm, CommentForm
+from .models import Post, Comment
+
+
+post_list = ListView.as_view(model=Post)
+
+post_detail = DetailView.as_view(model=Post)
 
 
 def post_new(request):
@@ -15,6 +21,69 @@ def post_new(request):
             return redirect(success_url)
     else:
         form = form_cls()
+
+    return render(request, template_name, {
+        'form': form,
+    })
+
+
+def post_edit(request, pk):
+    form_cls = PostForm
+    template_name = 'myapp/post_form.html'  # CreateView CBV의 Rule대로 지정한 것
+    success_url = '/'
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = form_cls(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect(success_url)
+    else:
+        form = form_cls(instance=post)
+
+    return render(request, template_name, {
+        'form': form,
+    })
+
+
+def comment_new(request, post_pk):
+    form_cls = CommentForm
+    template_name = 'myapp/comment_form.html'
+    success_url = '/'
+
+    post = get_object_or_404(Post, pk=post_pk)
+
+    if request.method == 'POST':
+        form = form_cls(request.POST, request.FILES)
+        if form.is_valid():
+            comment = form.save(commit=False)  # author, message
+            comment.post = post
+            comment.ip = request.META['REMOTE_ADDR']
+            comment.save()
+            return redirect(success_url)  # post_detail
+    else:
+        form = form_cls()
+
+    return render(request, template_name, {
+        'form': form,
+    })
+
+
+def comment_edit(request, post_pk, pk):
+    form_cls = CommentForm
+    template_name = 'myapp/comment_form.html'
+    success_url = '/'
+
+    comment = get_object_or_404(Comment, pk=pk)
+
+    if request.method == 'POST':
+        form = form_cls(request.POST, request.FILES, instance=comment)
+        if form.is_valid():
+            comment = form.save()
+            return redirect(success_url)  # TODO: post detail
+    else:
+        form = form_cls(instance=comment)
 
     return render(request, template_name, {
         'form': form,
